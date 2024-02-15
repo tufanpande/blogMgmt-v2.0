1916b0b38dca9eeaea5d63537c4f006a267655f7
Tufan <tufanpande@gmail.com>
2024-02-15 15:10:03 +0545

    model

--- modules/users/user.model.js
+++ modules/users/user.model.js
@@ -1,18 +1,20 @@
-const {Schema, model}=require("mongoose");
-
-
-
-const {objectId}= Schema.Types;
+const { Schema, model } = require("mongoose");
+const { ObjectId } = Schema.Types;
 
 const userSchema = new Schema({
-    name:{type:String ,required:true},
-    email:{type:String,required:true , unique:true},
-    password:{type:String,required:true, select:false},
-    roles:{type:String,enum:["admin","user"],default:"user"},
-    isActive:{type:Boolean,required:true, default:true},
-    createdAt:{type:Date,default:Date.now},
-    updatedAt:{type:Date,default:Date.now},
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, select: false },
+  roles: {
+    type: [String],
+    enum: ["admin", "user"],
+    default: "user",
+    required: true,
+  },
+  token: String,
+  isActive: { type: Boolean, required: true, default: true },
+  createdAt: { type: Date, default: Date.now() },
+  updatedAt: { type: Date, default: Date.now() },
 });
-
 
 module.exports = new model("User", userSchema);
