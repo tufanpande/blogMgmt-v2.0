95075dcc039c720fec0389fa8d4a1409598e4024
Tufan <tufanpande@gmail.com>
2024-03-01 17:08:08 +0545

    Token Generator

--- utils/token.js
+++ utils/token.js
@@ -1,17 +1,21 @@
-const JWT =require("jsonwebtoken");
+require("dotenv").config();
+const JWT = require("jsonwebtoken");
 
-const signJWT =(payload)=>{
-    return JWT.sign(
-        {
-            data:payload,
-        },
-        process.env.JWT_SECRET,
-        {expiresIn:process.env.JWT_DUTATION}
-    );
+const signJWT = (payload) => {
+  return JWT.sign(
+    {
+      data: payload,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_DURATION }
+  );
 };
 
-const verifyJWT =(token)=>{
-    return JWT.verify(token, process.env.JWT_SECRET);
+const verifyJWT = (token) => {
+  return JWT.verify(token, process.env.JWT_SECRET);
 };
 
-module.exports ={signJWT,verifyJWT};
+const generateRandomToken = () => {
+  return Math.floor(100000 + Math.random() * 900000);
+};
+module.exports = { signJWT, verifyJWT, generateRandomToken };
